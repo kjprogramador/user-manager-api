897f2a8b858f148c7925ec646bb5693adc03d667
Kevin Leon G <kjlengo@gmail.com>
2024-09-18 16:04:21 -0500

    Refacctorizando middleware de autenticación: recibiendo token en encabezado Authorization

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,10 +1,14 @@
 import jwt from "jsonwebtoken";
 
 export const validateToken = (req, res, next) => {
-  const token = req.header("x-auth-token");
-  if (!token) {
-    return res.status(401).json({ mensaje: "No hay token, permiso denegado" });
+  const authHeader = req.header("Authorization");
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ mensaje: "No hay token, permiso no válido" });
   }
+
+  // Extraer el token después de "Bearer "
+  const token = authHeader.split(" ")[1];
 
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET);
